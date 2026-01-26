e74b737cdb1c2e3075b061d032a8da00d4ec97d0
johnsonsuna <30959913+johnsonsuna@users.noreply.github.com>
2026-01-25 21:28:42 -0800

    feature: add backend CI using Step 2.3 test script (#2)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ ! -x "./.venv/bin/python" ]; then
+  python3 -m venv .venv
+fi
+
+./.venv/bin/python -m pip install -r requirements.txt
 ./.venv/bin/python -m pytest -q
